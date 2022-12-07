0a18e9edef4fae735edbda09fb5e40392dfdeefa
Jonathan Leon <jmartinezz1393@gmail.com>
2022-12-07 07:45:17 -0500

    script.js

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -1,22 +1,45 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-for(var i = 0; i <= generateBtn; i++){
-  password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-
-}
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();  
+  alert("GENERATE PASSWORD!!!!!!!!!!")
+  alert("length of at least 8 characters")
+  alert("lowercase, uppercase, numeric, and/or special characters")
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+function generatePassword() {
+  let char =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&()*+-./:{}[]";
+  var passwordLength = 12;
+  let password ="";
+
+
+  for (let i = 0; i < char.length; i++) {
+    let randomNumber = Math.floor(Math.random() * char.length);
+    password += char.substring(randomNumber, randomNumber + 80);
+    
+    console.log(password);
+    return password;
+  }
+  
+  document.getElementById("password").value = password;
+}
+
+
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
